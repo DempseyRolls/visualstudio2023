6710d34451f9eaef67c26507c6466a47c292f1d6
DempseyRolls <adolfo.martinez04@inacapmail.cl>
2023-03-21 11:30:50 -0300

    ejercicio1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            Console.WriteLine("Ingrese Nombre");
+            //esto es un scanner
+            string nombre = Console.ReadLine();
+            //Console.WriteLine("su nombre es " + nombre);
+            Console.WriteLine("ingrese al edad");
+            string edad = Console.ReadLine();
+            Console.WriteLine("su nombre es {0} y la edad es {1}",nombre,edad);
+            //esta es otra forma de leer datos
+            Console.ReadKey();
+            
+        }
+    }
+}
